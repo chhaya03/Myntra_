2e028be8b01a834305cc96cbd3bf809872b06a12
Chhaya Mishra <141479837+chhaya03@users.noreply.github.com>
2025-04-10 11:21:51 +0530

    Update app.js

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -43,4 +43,8 @@ app.post('/items', async (req, res) => {
   res.status(201).json({ message: 'Stored new item.', item: newItem });
 });
 
-app.listen(8080);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
+
